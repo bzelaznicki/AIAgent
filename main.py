d4bf9524de4974fbabe2a5639101ca166d5b810d
bzelaznicki <bartosz@zelaznicki.com>
2025-06-04 11:13:22 +0200

    adding verbose mode

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 load_dotenv()
@@ -12,20 +13,28 @@ def main():
     args = sys.argv[1:]
     if not args:
         raise Exception("The prompt cannot be empty")
-    
+    verbose = args.__contains__("--verbose")
     prompt = " ".join(args)
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)]),
+]
+    if verbose:
+        print(f"Working on: {prompt}")
+    generate_content(client, messages, verbose)
 
+
+
+def generate_content(client, messages, verbose):
     try:
         response = client.models.generate_content(
-            model='gemini-2.0-flash-001', contents=prompt
+            model='gemini-2.0-flash-001', contents=messages
         )
     except Exception as err:
-        print(f"Error encountered: {err}")
-        
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+        print(f"Error encountered: {err}")    
+    if verbose: 
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")
     print(response.text)
-
 if __name__ == "__main__":
     main()
